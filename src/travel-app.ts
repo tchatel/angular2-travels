d9049bb5fc9c9bd12ddc36a11e7b27ee2c85d454
Thierry Chatel <tchatel@gmail.com>
2015-11-04 12:27:19 +0100

    intial state

--- src/travel-app.ts
+++ src/travel-app.ts
@@ -3,16 +3,13 @@ import {
     Component
 } from 'angular2/angular2';
 
-import { TravelList } from './travel-list'
-
 
 @Component({
     selector: 'travel-app',
     template: `
         <h1>Angular 2 : Sample Travels Application</h1>
-        <travel-list></travel-list>
     `,
-    directives: [TravelList]
+    directives: []
 })
 export class TravelApp {
     constructor() {
--- src/travel-list.ts
+++ src/travel-list.ts
@@ -1,53 +1,34 @@
-import {
-    Component,
-    CORE_DIRECTIVES
-} from 'angular2/angular2';
 
-import { Travel } from './travel'
+/*
+<h2>Travels List:</h2>
+<table class="pure-table pure-table-horizontal" id="travel-list">
+    <thead>
+        <tr>
+            <td>Picture</td><td>Place</td><td>Country</td><td>Year</td><td></td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr *ng-for="#travel of travels">
+            <td>***</td>
+            <td>***</td>
+            <td>***</td>
+            <td>***</td>
+            <td>
+                <a href class="pure-button">
+                    <i class="fa fa-trash-o"></i> Remove
+                </a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+*/
 
-
-@Component({
-    selector: 'travel-list',
-    template: `
-        <h2>Travels List:</h2>
-        <table class="pure-table pure-table-horizontal" id="travel-list">
-            <thead>
-                <tr>
-                    <td>Picture</td><td>Place</td><td>Country</td><td>Year</td><td></td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr *ng-for="#travel of travels">
-                    <td><img src="img/{{travel.img || 'none.jpg'}}"/></td>
-                    <td [ng-class]="{old: travel.isOld()}">{{travel.place}}</td>
-                    <td>{{travel.country}}</td>
-                    <td>{{travel.year}}</td>
-                    <td>
-                        <a href (click)="remove(travel)" class="pure-button">
-                            <i class="fa fa-trash-o"></i> Remove
-                        </a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    `,
-    directives: [CORE_DIRECTIVES]
-})
-export class TravelList {
-    travels: Travel[];
-    selectedTravel: Travel;
-    constructor() {
-        this.travels = [
-            new Travel("SF2015", "San Francisco", "USA", 2015, "sanfrancisco.jpg"),
-            new Travel("NA2014", "Nantes", "France", 2014, "nantes.jpg"),
-            new Travel("BX2014", "Bruxelles", "Belgique", 2014, "bruxelles.jpg"),
-            new Travel("YO2014", "Yosemite", "USA", 2014, "yosemite.jpg"),
-            new Travel("BT2013", "Bretagne", "France", 2013, "bretagne.jpg")
-        ];
-    }
-    remove(travel: Travel) {
-        let index = this.travels.indexOf(travel);
-        this.travels.splice(index, 1);
-        return false;
-    }
-}
+/*
+    [
+        new Travel("SF2015", "San Francisco", "USA", 2015, "sanfrancisco.jpg"),
+        new Travel("NA2014", "Nantes", "France", 2014, "nantes.jpg"),
+        new Travel("BX2014", "Bruxelles", "Belgique", 2014, "bruxelles.jpg"),
+        new Travel("YO2014", "Yosemite", "USA", 2014, "yosemite.jpg"),
+        new Travel("BT2013", "Bretagne", "France", 2013, "bretagne.jpg")
+    ]
+*/
