b30334fc2acf36a126e3a1eb53dec7fcca84d21e
Thierry Chatel <tchatel@gmail.com>
2015-11-04 10:07:01 +0100

    branch E: router

--- src/travel-edit.ts
+++ src/travel-edit.ts
@@ -1,45 +1,22 @@
-import {
-    Component,
-    FORM_DIRECTIVES
-} from 'angular2/angular2';
-import {
-    ROUTER_DIRECTIVES,
-    RouteParams
-} from 'angular2/router';
-
-import { Travel } from './travel'
-import { TravelManager } from './travel-manager'
-
-
-@Component({
-    selector: 'travel-edit',
-    template: `
-        <h2>Edit travel: {{travel.place}}</h2>
-        <form class="pure-form pure-form-aligned">
-            <fieldset>
-                <div class="pure-control-group">
-                    <label for="name">PLace</label>
-                    <input id="name" type="text" [(ng-model)]="travel.place"/>
-                </div>
-                <div class="pure-control-group">
-                    <label for="email">Country</label>
-                    <input id="email" type="text" [(ng-model)]="travel.country"/>
-                </div>
-                <div class="pure-control-group">
-                    <label for="phone">Year</label>
-                    <input id="phone" type="number" [(ng-model)]="travel.year"/>
-                </div>
-                <div class="pure-controls">
-                    <a [router-link]="['/List']" class="pure-button pure-button-primary">Return to list</a>
-                </div>
-            </fieldset>
-        </form>
-    `,
-    directives: [FORM_DIRECTIVES, ROUTER_DIRECTIVES]
-})
-export class TravelEdit {
-    travel: Travel;
-    constructor(cm: TravelManager, params: RouteParams) {
-        this.travel = cm.find(params.get('id'));
-    }
-}
+/*
+<h2>Edit travel: ****</h2>
+<form class="pure-form pure-form-aligned">
+    <fieldset>
+        <div class="pure-control-group">
+            <label for="name">PLace</label>
+            <input id="name" type="text"/>
+        </div>
+        <div class="pure-control-group">
+            <label for="email">Country</label>
+            <input id="email" type="text"/>
+        </div>
+        <div class="pure-control-group">
+            <label for="phone">Year</label>
+            <input id="phone" type="number"/>
+        </div>
+        <div class="pure-controls">
+            <a class="pure-button pure-button-primary">Return to list</a>
+        </div>
+    </fieldset>
+</form>
+*/
