19a9b8d60b1e8055af9c298f7822d816488be95e
Thierry Chatel <tchatel@gmail.com>
2015-11-04 10:17:03 +0100

    branch E: router

--- src/travel-manager.ts
+++ src/travel-manager.ts
@@ -19,11 +19,11 @@ export class SampleTravelManager extends TravelManager {
     constructor() {
         super();
         this.list.push(
-            new Travel("SF2015", "San Francisco", "USA", "2015", "sanfrancisco.jpg"),
-            new Travel("NA2014", "Nantes", "France", "2014", "nantes.jpg"),
-            new Travel("BX2014", "Bruxelles", "Belgique", "2014", "bruxelles.jpg"),
-            new Travel("YO2014", "Yosemite", "USA", "2014", "yosemite.jpg"),
-            new Travel("BT2013", "Bretagne", "France", "2013", "bretagne.jpg")
+            new Travel("SF2015", "San Francisco", "USA", 2015, "sanfrancisco.jpg"),
+            new Travel("NA2014", "Nantes", "France", 2014, "nantes.jpg"),
+            new Travel("BX2014", "Bruxelles", "Belgique", 2014, "bruxelles.jpg"),
+            new Travel("YO2014", "Yosemite", "USA", 2014, "yosemite.jpg"),
+            new Travel("BT2013", "Bretagne", "France", 2013, "bretagne.jpg")
         );
     }
 }
